0fd60abb3e9e86dbc27ba81263843686e435e817
Wrexan <fwrexan@gmail.com>
2022-05-31 12:01:37 +0300

    done big part of templating engine, framework and site

--- framework/views.py
+++ framework/views.py
@@ -1,3 +1,5 @@
+import sys, os
+
 
 class View:
     def __init__(self,
@@ -25,105 +27,115 @@ class View:
             for var, link in self.frontend_vars.items():
                 data = data.replace(var, link)
         except Exception as e:
-            print(f'ERROR: while parsing view: ({page_file=}) {e}')
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(f'{fname}[{exc_tb.tb_lineno}]ERROR: while parsing view: ({page_file=}) {e}')
             return page_file
         return data
 
     # Костыли с рекурсией. Нагугленные способы импорта на сервере не работают
     def _just_inject_in_that_file(self, data, injections, params, file, num: int = 0):
-        layer = 0
-        inj_end = 0
-        # num = 0
-        # key = ''
-        while layer < self.deepness:
-            # print(f'Using layer {layer} of {self.deepness}')
-            inj_start = data.find('{{', inj_end)
-            if inj_start >= 0:
-                inj_end = data.find('}}', inj_start)
-                if inj_end == -1:
-                    raise Exception(f'ERROR: not found "}}" in {file}')
-                inj_var = data[inj_start + 2: inj_end]
-                if len(inj_var) <= 1:
-                    # inj_end = inj_start + 5
-                    continue
-                # inj_end = inj_start + 2
-                # print(f'{inj_var=} {inj_start=} {inj_end=}')
-                # cycle_pos = inj_var.find('*')
-                # if cycle_pos >= 0:  # If * found, then injection will repeat
-                #     cycle_end = data.find('{{*}}', inj_end)
-                #     cycles = int(inj_var[:cycle_pos])
-                #     cycle_var = inj_var[inj_repeat + 1:]
-                #
-                #     # print(f'{inj_repeat=}')
-                #     new_inj_var = inj_var[:inj_repeat]
-                #     # print(f'{inj_var=}')
-                #     inj_repeat = int(inj_var[inj_repeat + 1:])
-                #     # print(f'{inj_repeat=}')
-                #     inj_var = new_inj_var
-                inj_arg = None
-                # if injections:
-                if inj_var in injections.keys():
-                    inj_arg = injections[inj_var]
-                else:
-                    dot_pos = inj_var.find(':')
-                    if dot_pos >= 0:  # If . found, then injection parse dict
-                        key = inj_var[:dot_pos]
-                        arg = inj_var[dot_pos + 1:]
-                        # print(f'{key=} {arg=}')
-                        if key in injections.keys():
-                            # print(f'{injections=}')
-                            inj_arg = injections[key]
-                            # print(f'{inj_arg=}')
-                    dot_pos = inj_var.find('#')
-                    if dot_pos >= 0:  # If . found, then injection parse dict
-                        key = inj_var[:dot_pos]
-                        num = int(inj_var[dot_pos + 1:])
-                        # print(f'{key=} {num=}')
-                        if key in injections.keys():
-                            # print(f'{injections=}')
-                            inj_arg = injections[key]
-                            # print(f'{inj_arg=}')
-                # print(f'{inj_var=} {inj_arg=} {type(inj_arg)=}')
-                if inj_arg:
-                    # inj_arg = injections[inj_var]
-                    file = inj_arg
-                    # print(f'{inj_var=} {inj_arg=} {type(inj_arg)=}')
-                    if isinstance(inj_arg, str):
-                        if inj_arg[-5:] == '.html':  # If str.html, then filename
-                            inj_data = self._open_file(f'{self.frontend_path}{inj_arg}')
-                        else:
-                            inj_data = inj_arg
-                    elif isinstance(inj_arg, list):
-                        if num < len(inj_arg):
-                            # print(f'{num=} {inj_arg[num]["id"]=}')
-                            inj_data = inj_arg[num][arg]
-                            # print(f'{inj_arg[num][arg]=}')
-                        else:
-                            inj_data = ''
-
-                    elif isinstance(inj_arg, dict):
-                        print('dict')
-                    elif isinstance(inj_arg, tuple):
-                        print('tuple')
+        try:
+            layer = 0
+            inj_end = 0
+            data = str(data) if isinstance(data, int) else data
+            # num = 0
+            # key = ''
+            while layer < self.deepness:
+                # print(f'Using layer {layer} of {self.deepness}')
+                inj_start = data.find('{{', inj_end)
+                if inj_start >= 0:
+                    inj_end = data.find('}}', inj_start)
+                    if inj_end == -1:
+                        raise Exception(f'ERROR: not found "}}" in {file}')
+                    inj_var = data[inj_start + 2: inj_end]
+                    if len(inj_var) <= 1:
+                        # inj_end = inj_start + 5
+                        continue
+                    # inj_end = inj_start + 2
+                    # print(f'{inj_var=} {inj_start=} {inj_end=}')
+                    # cycle_pos = inj_var.find('*')
+                    # if cycle_pos >= 0:  # If * found, then injection will repeat
+                    #     cycle_end = data.find('{{*}}', inj_end)
+                    #     cycles = int(inj_var[:cycle_pos])
+                    #     cycle_var = inj_var[inj_repeat + 1:]
+                    #
+                    #     # print(f'{inj_repeat=}')
+                    #     new_inj_var = inj_var[:inj_repeat]
+                    #     # print(f'{inj_var=}')
+                    #     inj_repeat = int(inj_var[inj_repeat + 1:])
+                    #     # print(f'{inj_repeat=}')
+                    #     inj_var = new_inj_var
+                    inj_arg, key = None, None
+                    # if injections:
+                    if inj_var in injections.keys():
+                        inj_arg = injections[inj_var]
                     else:
-                        print(f'ATTENTION: unknown type var: ({inj_var=} {type(inj_arg)=})'
-                              f' in ({file if isinstance(file, str) else "injection"})')
-                        inj_data = ''
-                elif inj_var[-5:] == '.html':  # If not variable, then filename
-                    inj_data = self._open_file(f'{self.frontend_path}{inj_var}')
-                else:
-                    print(f'ATTENTION: unused var: ({inj_var=}) in ({file if isinstance(file, str) else "injection"})')
-                    inj_data = ''
-                    # raise Exception
-                    # continue
+                        dot_pos = inj_var.find(':')
+                        if dot_pos >= 0:  # If . found, then injection parse dict
+                            obj = inj_var[:dot_pos]
+                            key = inj_var[dot_pos + 1:]
+                            # print(f'{key=} {arg=}')
+                            if obj in injections.keys():
+                                # print(f'{injections=}')
+                                inj_arg = injections[obj]
+                                # print(f'{inj_arg=}')
+                        dot_pos = inj_var.find('#')
+                        if dot_pos >= 0:  # If . found, then injection parse dict
+                            obj = inj_var[:dot_pos]
+                            num = int(inj_var[dot_pos + 1:])
+                            # print(f'{key=} {num=}')
+                            if obj in injections.keys():
+                                # print(f'{injections=}')
+                                inj_arg = injections[obj]
+                                # print(f'{inj_arg=}')
+                    # print(f'{inj_var=} {type(inj_arg)=}')
+                    # print(f'{inj_arg=}')
+                    if inj_arg:
+                        # inj_arg = injections[inj_var]
+                        file = inj_arg
+                        # print(f'{inj_var=} {inj_arg=} {type(inj_arg)=}')
+                        if isinstance(inj_arg, str):
+                            if inj_arg[-5:] == '.html':  # If str.html, then filename
+                                inj_data = self._open_file(f'{self.frontend_path}{inj_arg}')
+                            else:
+                                inj_data = inj_arg
+                        elif isinstance(inj_arg, list):
+                            if num < len(inj_arg):
+                                # print(f'{num=} {inj_arg[num]["id"]=}')
+                                inj_data = inj_arg[num][key]
+                                # print(f'{inj_arg[num][arg]=}')
+                            else:
+                                inj_data = ''
 
-                if layer < self.deepness:
-                    inj_data = self._just_inject_in_that_file(inj_data, injections, params, file, num)
-                data = data[:inj_start] + inj_data + data[inj_end + 2:]
-                # print(f'{data =}')
-                # print(f'{inj_var=} {inj_file=}')
-            else:
-                layer += 1
+                        elif isinstance(inj_arg, dict):
+                            if key in inj_arg:
+                                inj_data = inj_arg[key]
+                        elif isinstance(inj_arg, tuple):
+                            print('tuple')
+                        else:
+                            print(f'ATTENTION: unknown type var: ({inj_var=} {type(inj_arg)=})'
+                                  f' in ({file if isinstance(file, str) else "injection"})')
+                            inj_data = ''
+                    elif inj_var[-5:] == '.html':  # If not variable, then filename
+                        inj_data = self._open_file(f'{self.frontend_path}{inj_var}')
+                    else:
+                        print(f'ATTENTION: unused var: ({inj_var=}) in ({file if isinstance(file, str) else "injection"})')
+                        inj_data = ''
+                        # raise Exception
+                        # continue
+
+                    if layer < self.deepness:
+                        inj_data = self._just_inject_in_that_file(inj_data, injections, params, file, num)
+                    data = data[:inj_start] + inj_data + data[inj_end + 2:]
+                    # print(f'{data =}')
+                    # print(f'{inj_var=} {inj_file=}')
+                else:
+                    layer += 1
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(f'{fname}[{exc_tb.tb_lineno}]ERROR: while parsing view: ({file=}) {e}')
         return data
 
     def _open_file(self, path):
--- views.py
+++ views.py
@@ -49,16 +49,34 @@ def courses(request):
                                             'courses': 'courses_invitation.html'})
         else:
             if 'edu_line' in request.query_params:
-                # print(f'{request.query_params["edu_line"]}')
                 courses_of_line = db_get_courses_by_line(request.query_params["edu_line"])
+                # print(f'{courses_of_line=}')
                 page = Home.view('index.html', {'content': 'courses_page.html',
                                                 'courses': 'courses.html',
                                                 'edu_line1': db_get_line(1)['name'],
                                                 'edu_line2': db_get_line(2)['name'],
                                                 'edu_line3': db_get_line(3)['name'],
                                                 'course': courses_of_line,
+                                                'course_page': 'course_page.html',
                                                 'course_form': 'course_form.html'},
                                  request.query_params["edu_line"])
+            if 'course_page' in request.query_params:
+                course = db_get_course(request.query_params["course_page"])
+                # print(f'{course=}')
+                page = Home.view('index.html', {'content': 'course_page.html',
+                                                'course': course},
+                                 request.query_params["course_page"])
+            if 'enroll_course' in request.query_params:
+                course = db_get_course(request.query_params["enroll_course"])
+                print(f'Клиент записан на {course["name"]}')
+                page = Home.view('index.html', {'content': 'thank_for_enroll.html',
+                                                'course': course})
+                # page = Home.view('index.html', {'content': 'courses_page.html',
+                #                                 'edu_line1': db_get_line(1)['name'],
+                #                                 'edu_line2': db_get_line(2)['name'],
+                #                                 'edu_line3': db_get_line(3)['name'],
+                #                                 'course': course,
+                #                                 'courses': 'thank_for_enroll.html'})
     elif request.method == 'POST':
         page = Home.view('courses.html')
         print(f'==Got {request.method=} {request.query_params=} {request.body=}')
